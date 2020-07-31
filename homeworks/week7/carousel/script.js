3f4257615fad7f4ed5308fe8b92894aaa5721a1d
JAS0NHUANG <jas0nhuang@tuta.io>
2020-07-31 16:05:31 -0200

    fix a carouel bug thanks to @ahwei777

--- homeworks/week7/carousel/script.js
+++ homeworks/week7/carousel/script.js
@@ -22,17 +22,23 @@ document.querySelector('.carousel').addEventListener('click', evt => {
 function carousel(currentIndex, direction) {
   const opposite = direction === 'right' ? 'left' : 'right'
   const nextIndex = direction === 'right' ? rightImg : leftImg
+  // 新增下面一行刪掉前一個圖片裡的 opposite class name
+  document.querySelector(`.${opposite}`).classList.remove(opposite)
   imgList[currentIndex].className = `carousel__img ${opposite}`
   imgList[nextIndex].className = 'carousel__img current'
   currentImg = nextIndex
 
   if (direction === 'right') {
     rightImg = (rightImg + 1 > imgList.length - 1) ? 0 : rightImg + 1
+    // 新增下面一行改變反方向 Img 的 Index
+    leftImg = (leftImg + 1 > imgList.length - 1) ? 0 : leftImg + 1
     imgList[rightImg].className = 'carousel__img right'
   }
 
   if (direction === 'left') {
     leftImg = (leftImg - 1 < 0) ? imgList.length - 1 : leftImg - 1
+    // 新增下面一行改變反方向 Img 的 Index
+    rightImg = (rightImg - 1 < 0) ? imgList.length - 1 : rightImg - 1
     imgList[leftImg].className = 'carousel__img left'
   }
 }
